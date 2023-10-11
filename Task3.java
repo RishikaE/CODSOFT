e873e550d371b2901ff874165d43ce8c7895ab37
Rishika E <119498804+RishikaE@users.noreply.github.com>
2023-10-11 11:09:35 +0530

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,107 @@
+package atmInterface;
+
+import java.util.Scanner;
+
+class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
+
+class BankAccount{
+	private double balance;
+	
+	public BankAccount(double  initialBalance) {
+		balance = initialBalance;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public boolean deposit(double amount) {
+		if(amount > 0) 
+			balance += amount;
+		else 
+	         throw new IllegalArgumentException("Invalid deposit amount.");
+			return true;
+		}
+		
+	
+	public boolean withdraw(double amount) throws InsufficientFundsException{
+		if(amount > 0 && amount <= balance) 
+			balance -= amount;
+		
+		else 
+	           throw new InsufficientFundsException("Invalid withdrawal amount or insufficient balance.");
+		return true;
+		
+	}    
+}
+
+class ATM{
+	private BankAccount userAccount;
+	
+	public ATM(BankAccount account) {
+		userAccount = account;
+	}
+	
+	public void displayMenu() {
+		System.out.println("Choose your option :");
+		System.out.println("1. Check Balance");
+		System.out.println("2. Deposit");
+		System.out.println("3. Withdraw");
+		System.out.println("4. Exit");
+	}
+	
+	public void processTrascation() {
+		Scanner scanner = new Scanner(System.in);
+		int choice;
+		do {
+			displayMenu();
+			System.out.print("Enter Your Choice : ");
+			choice = scanner.nextInt();
+		
+		try {
+			switch(choice) {
+				case 1:
+					System.out.println("Current balance : "+userAccount.getBalance());
+					break;
+				case 2:
+					System.out.println("Enter deposit amount : ");
+					double depositAmount = scanner.nextDouble();
+					if(userAccount.deposit(depositAmount))
+						System.out.println("Deposit successful. New Balance : "+userAccount.getBalance());
+					else
+						System.out.println("Invalid deposit amount.");
+					
+					break;
+				case 3:
+					System.out.println("Enter the withdrawal amount : ");
+					double withdrawalAmount = scanner.nextDouble();
+					if(userAccount.withdraw(withdrawalAmount))
+						System.out.println("Withdrawal successful.. New Balance : "+userAccount.getBalance());
+					else
+						System.out.println("Invalid withdrawal amount or insufficient balance.");
+					
+					break;
+				case 4:
+					System.out.println("Thank You :) for using the ATM!");
+					break;
+				default:
+					System.out.println("Invalid choice. Please select a valid option.");
+			}	
+		}catch (IllegalArgumentException | InsufficientFundsException e) {
+             System.out.println("Error: " + e.getMessage());
+            }
+	}while(choice != 4);
+}
+}	
+class Task3 {
+	public static void main(String[] args) {
+		BankAccount userAccount = new BankAccount(1000);
+		ATM atm = new ATM(userAccount);
+		atm.processTrascation();
+	}
+
+}
